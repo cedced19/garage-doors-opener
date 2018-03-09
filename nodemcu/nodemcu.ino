72f6a2815e4c2771568f9fb94da84c4d00585ca2
Cédric JUNG <cedced19@gmail.com>
2018-03-09 18:36:55 +0100

    Add program for nodemcu

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,17 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+
 const char* ssid = "wifi_name"; // wifi SSID
 const char* password = "wifi_pwd";// wifi password
-#define garage1 0
-#define garage2 2
+#define garageCommand1 14 // D5
+#define garageSensor1 12 // D6
+#define garageCommand2 13 // D7
+#define garageSensor2 15 // D8
 ESP8266WebServer server(80);
-MDNSResponder mdns;
+
 void setup(void){
-  pinMode(garage1, INPUT);
-  pinMode(garage2, INPUT);
-  Serial.begin(9600);
+  pinMode(garageSensor1, INPUT);
+  pinMode(garageSensor2, INPUT);
+  pinMode(garageCommand1, OUTPUT);
+  pinMode(garageCommand2, OUTPUT);
+  digitalWrite(garageCommand1, HIGH);
+  digitalWrite(garageCommand2, HIGH);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -22,16 +27,20 @@ void setup(void){
 
   server.on("/garage/1/toggle/oGXabhvW6k", [](){
     server.send(200, "application/json", "{\"toggling\":true}");
-    Serial.println('1');
+    digitalWrite(garageCommand1, LOW);   
+    delay(1000);                      
+    digitalWrite(garageCommand1, HIGH);
   });
 
   server.on("/garage/2/toggle/oGXabhvW6k", [](){
     server.send(200, "application/json", "{\"toggling\":true}");
-    Serial.println('2');
+    digitalWrite(garageCommand2, LOW);   
+    delay(1000);                      
+    digitalWrite(garageCommand2, HIGH);
   });
 
   server.on("/garage/1/status/oGXabhvW6k", [](){
-    if (digitalRead(garage1) == HIGH) {
+    if (digitalRead(garageSensor1) == HIGH) {
       server.send(200, "application/json", "{\"closed\":true}");
     } else {
       server.send(200, "application/json", "{\"closed\":false}");
@@ -40,7 +49,7 @@ void setup(void){
   });
 
   server.on("/garage/2/status/oGXabhvW6k", [](){
-    if (digitalRead(garage2) == HIGH) {
+    if (digitalRead(garageSensor2) == HIGH) {
       server.send(200, "application/json", "{\"closed\":true}");
     } else {
       server.send(200, "application/json", "{\"closed\":false}");
