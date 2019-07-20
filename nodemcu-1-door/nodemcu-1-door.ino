c454052273ff793dc5ca91fed51660e0126375ca
Cédric JUNG <cedced19@gmail.com>
2019-07-20 15:57:16 +0200

    Add code for 1 garage

--- nodemcu-1-door/nodemcu-1-door.ino
+++ nodemcu-1-door/nodemcu-1-door.ino
@@ -4,21 +4,16 @@
 
 const char* ssid = "wifi_name"; // wifi SSID
 const char* password = "wifi_pwd";// wifi password
-#define garageCommand1 14 // D5
-#define garageSensor1 12 // D6
-#define garageCommand2 05 // D1
-#define garageSensor2 04 // D2
+#define garageCommand1 05 // D1
+#define garageSensor1 14 // D5
 ESP8266WebServer server(80);
 
 void setup(void){
   pinMode(2, OUTPUT); 
   delay(2000);
   pinMode(garageSensor1, INPUT);
-  pinMode(garageSensor2, INPUT);
   pinMode(garageCommand1, OUTPUT);
-  pinMode(garageCommand2, OUTPUT);
   digitalWrite(garageCommand1, HIGH);
-  digitalWrite(garageCommand2, HIGH);
 
   WiFi.begin(ssid, password);
   
@@ -31,31 +26,15 @@ void setup(void){
   WiFi.mode(WIFI_STA);
 
 
-  server.on("/garage/1/toggle/oGXabhvW6k", [](){
+  server.on("/garage/1/toggle/4czODA", [](){
     server.send(200, "application/json", "{\"toggling\":true}");
     digitalWrite(garageCommand1, LOW);   
     delay(1000);                      
     digitalWrite(garageCommand1, HIGH);
   });
 
-  server.on("/garage/2/toggle/oGXabhvW6k", [](){
-    server.send(200, "application/json", "{\"toggling\":true}");
-    digitalWrite(garageCommand2, LOW);   
-    delay(1000);                      
-    digitalWrite(garageCommand2, HIGH);
-  });
-
-  server.on("/garage/1/status/oGXabhvW6k", [](){
+  server.on("/garage/1/status/4czODA", [](){
     if (digitalRead(garageSensor1) == HIGH) {
-      server.send(200, "application/json", "{\"closed\":true}");
-    } else {
-      server.send(200, "application/json", "{\"closed\":false}");
-    }
-    
-  });
-
-  server.on("/garage/2/status/oGXabhvW6k", [](){
-    if (digitalRead(garageSensor2) == HIGH) {
       server.send(200, "application/json", "{\"closed\":true}");
     } else {
       server.send(200, "application/json", "{\"closed\":false}");
