faaf58915bb69a6147bea69bf707758717cba4f2
Cédric JUNG <cedced19@gmail.com>
2019-08-19 19:48:39 +0200

    Add UI Todo add sunset api

--- util-clock/public/javascripts/admin/controllers/clocks.js
+++ util-clock/public/javascripts/admin/controllers/clocks.js
@@ -12,16 +12,51 @@ module.exports = ['$scope', '$http', '$rootScope', 'notie', '$location', functio
         }
     };
     $scope.up = true;
- 
+
+    function getNextTrigger (name) {
+        var h = $scope.clocks[name].hour;
+        var m = $scope.clocks[name].minute;
+        if (name == 'morning') {
+            var date = new Date();
+            if (date.getHours() > h) {
+                date.setDate(date.getDate()+1);
+            }
+            date.setHours(Number(h));
+            date.setMinutes(Number(m));
+            $scope.clocks[name].nextTrigger = date;
+        } 
+        if (name == 'night') {
+            var tmpDate = new Date();
+            if (tmpDate.getHours() > h) {
+                tmpDate.setDate(date.getDate()+1);
+            }
+            tmpDate.setHours(Number(h));
+            tmpDate.setMinutes(Number(m));
+            // compare
+            $scope.clocks[name].nextTrigger = tmpDate;
+            
+        }
+    }
+
+    function updateNextTriggers() {
+        for (i in $scope.clocks) {
+            getNextTrigger(i);
+        }
+    } 
 
     $http.get('/api/clock').success(function (data) {
-
+        updateNextTriggers();
     }).error($rootScope.$error);
 
-    $scope.updateMeta = function (name) {
+    updateNextTriggers();
+
+    $scope.updateClock = function (name) {
+        var h = $scope.clocks[name].hour;
+        var m = $scope.clocks[name].minute;
+        getNextTrigger(name);
         $http.put('/api/clocks/' + name, {
-                hours: $scope.clocks[name].hours,
-                minutes: $scope.clocks[name].minutes
+                hour: h,
+                minute: m
         }).success(function (data) {
             notie.alert(1, 'La minuterie a bien été modifié.', 3);
             $scope.clocks[name].nextTrigger=data.nextTrigger;
